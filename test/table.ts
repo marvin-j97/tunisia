cf0c7691814ebef6a2166ac65da1958ce503a295
marvin-j97 <marvin.janke.97@gmail.com>
2023-01-02 19:11:59 +0100

    ci: add logging

--- test/table.ts
+++ test/table.ts
@@ -16,21 +16,32 @@ export const db = new aws.DynamoDB(config);
 export const dynamoClient = new aws.DynamoDB.DocumentClient({
   ...config,
   convertEmptyValues: true,
+  logger: {
+    log: (...msg) => console.log(...msg),
+  },
 });
 export const tunisia = new Tunisia(config);
 
-export async function getTableSize(name: string) {
+/**
+ *
+ * @param name
+ * @returns
+ */
+export async function getTableSize(name: string): Promise<number> {
   const items = await tunisia.scan(name).all();
   return items.length;
 }
 
+/**
+ * Creates test table
+ */
 export function initTable(
   name: string,
   attributes?: aws.DynamoDB.AttributeDefinitions,
   schema?: aws.DynamoDB.KeySchema,
   indices?: aws.DynamoDB.GlobalSecondaryIndexList,
 ) {
-  return async function (...args: any[]) {
+  return async (): Promise<void> => {
     const { TableNames } = await db.listTables().promise();
     if (TableNames && TableNames.includes(name)) {
       console.log(`Deleting table ${name}`);
